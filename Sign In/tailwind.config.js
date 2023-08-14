b1796104ad1a20753709e441365ad36f94472e4e
Shubham Kushwaha <41871310+kushwahashubham@users.noreply.github.com>
2023-08-14 15:50:47 +0530

    tailwind configuration file

---
+++ Sign In/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'golden': '#DCCA87',
+      },
+    },
+  },
+  plugins: [],
+}
+
